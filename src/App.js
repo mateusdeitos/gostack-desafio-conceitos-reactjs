c8139cb9582e3e4d23298804ba3f54f81a90febb
sirmd <mate.deitos@hotmail.com>
2020-06-16 23:21:40 -0300

    Desafio 02 -> Conceitos de ReactJS

--- src/App.js
+++ src/App.js
@@ -1,30 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
+import { FiThumbsUp } from 'react-icons/fi'
 
 function App() {
+
+  const [repos, setRepos] = useState([]);
+  const [newRepoTitle, setNewRepoTitle] = useState('');
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepos(response.data);
+    });
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const newRepo = {
+      title: newRepoTitle,
+      url: "https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs",
+      techs: ["ReactJS", "NodeJS", "ReactNative"]
+    }
+
+    api.post('/repositories', newRepo).then(response => {
+      setRepos([...repos, response.data])
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const deletedRepoIndex = repos.findIndex(repo => repo.id === id);
+    repos.splice(deletedRepoIndex, 1);
+    api.delete(`/repositories/${id}`).then(
+      setRepos([...repos])
+    )
+  }
+
+  function handleInputChange(event) {
+    const { value } = event.target;
+    setNewRepoTitle(value);
+  }
+
+  function handleLike(id) {
+    const likedRepoIndex = repos.findIndex(repo => repo.id === id);
+
+    api.post(`/repositories/${id}/like`).then(response => {
+      repos[likedRepoIndex].likes = response.data.likes;
+      setRepos([...repos])
+    })
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+    <>
+      <div className="container">
+        <div>
+          <input
+            name="inputRepo"
+            type="text"
+            placeholder="Título do repositório"
+            onChange={handleInputChange} />
+          <button id="adicionar" onClick={handleAddRepository}>Adicionar</button>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+        </div>
+
+        <ul data-testid="repository-list">
+
+          {repos.map(repo => (
+            <li key={repo.id}>
+              <p>{repo.title}</p>
+
+              <button id="like" onClick={() => handleLike(repo.id)}><span>{<FiThumbsUp />}</span> {repo.likes}</button>
+              <button onClick={() => handleRemoveRepository(repo.id)}>Remover</button>
+            </li>
+
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
